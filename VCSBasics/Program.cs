b2d4252c8db8b755a2658629b067db7141a9931d
Tran, Ngoc Long <long.tran1@optum.com>
2021-07-14 21:45:17 -0400

    3rdcommit

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -6,7 +6,7 @@ namespace VCSBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellod!");
+            Console.WriteLine("Hellod!123");
         }
     }
 }
